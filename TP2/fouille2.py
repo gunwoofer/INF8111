f6f702af4f02e17b5ea47ec9feb458ba36b4dc50
Benjamin <heinen.benjamin@gmail.com>
2019-06-02 21:47:54 -0400

    avancement arbre de decision

--- TP2/fouille2.py
+++ TP2/fouille2.py
@@ -8,10 +8,42 @@ import bixi_dataset as ds
 import matplotlib.pyplot as plt
 import pandas as pd
 import statistics as stats
-
+import tensorflow as tf
+from tensorflow import keras
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras import backend as K
 from sklearn.metrics import f1_score
 
 from sklearn.tree import DecisionTreeClassifier
+
+def construct_layers():
+    # number of layers
+    classifier = keras.Sequential()
+    classifier.add(keras.layers.Dense(units=5, activation="relu", input_dim=188))
+    classifier.add(keras.layers.Dense(units=5, activation="relu"))
+
+    classifier.add(keras.layers.Dense(units=1, activation="sigmoid"))
+    classifier.compile(
+    optimizer='adamax',
+    loss='mean_squared_error',
+    metrics=["accuracy"])
+    return classifier
+
+
+def station_to_one_hot_vector(station_code):
+    dic_station_code = dict.fromkeys(station_code.tolist(), 0)
+    i = 1
+    for key, value in dic_station_code.items():
+        dic_station_code[key] = i
+        i += 1
+    list_one_hot = []
+    for k in range (len(station_code)):
+        y = dic_station_code[station_code[k]]
+        one_hot = [0] * i
+        one_hot[y-1] = 1
+        list_one_hot.append(one_hot)
+    return np.array(list_one_hot)
 
 def update_median(array):
     col_median = np.nanmean(array, axis=0)
@@ -41,34 +73,48 @@ train.head()
 test= pd.read_csv("data/test.csv")
 print(test.shape)
 test.head()
+
+_, _, id_test = ds.getData(ds.TEST_FILE_NAME, type_data='test')
+
+print('formater donnée')
 X_train = reformat_data(train.iloc[:,1:6].values).astype('float32')
 X_train = update_median(X_train)
 
+test_val = update_median(reformat_data(test.iloc[:,1:6].values).astype('float32'))
+station_code_test = test.iloc[:, 13].values
+
 station_code = train.iloc[:, 13].values
-dic_station_code = dict.fromkeys(station_code.tolist(), 0)
-i = 1
-for key, value in dic_station_code.items():
-    dic_station_code[key] = i
-    i += 1
-station_code_one_hot = [[0] * i] * len(station_code) 
-list_one_hot = []
-for k in range (len(station_code)):
-    y = dic_station_code[station_code[k]]
-    one_hot = [0] * i
-    one_hot[y-1] = 1
-    # station_code_one_hot[k][y-1] = 1 
-    list_one_hot.append(one_hot)
-array_one_hot_station_code = np.array(list_one_hot)
+
+
+####
+print('creer station code')
+station_code_one_hot = station_to_one_hot_vector(station_code)
+station_code_one_hot_test = station_to_one_hot_vector(station_code_test)
+
 volume = train.iloc[:, 15].values
 
-train_x = np.concatenate((X_train, array_one_hot_station_code), axis=1)
+print('concatene')
+train_x = np.concatenate((X_train, station_code_one_hot), axis=1)
+test_x = np.concatenate((test_val, station_code_one_hot_test), axis=1)
 
+print('arbre de decision')
 clf = DecisionTreeClassifier(random_state=0)
 clf.fit(train_x, volume)
 
-valid_pred = clf.predict(X_train)
+print('debut prediction')
+valid_pred = clf.predict(train_x)
 f_score = f1_score(volume, valid_pred)
 print('fscore : ' + str(f_score))
+
+classifier = KerasClassifier(build_fn=construct_layers, batch_size=20, epochs=1)
+classifier.fit(x=train_x, y=volume)
+pred = classifier.predict(train_x)
+f_score = f1_score(volume, pred)
+
+print('fscore neural network : ' + str(f_score))
+test_pred = clf.predict(test_x)
+ds.writeCsv(id_test, test_pred)
+
 # plt.hist(volume, range = (0, 1), bins = 2, color = 'blue',
 #             edgecolor = 'red')
 # plt.xlabel('volume')
